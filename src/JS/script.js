83111e0769c6126d3dc134e533603a2d1e512e02
Junior7Marques <juniorjr07@live.com>
2023-07-09 12:28:10 -0300

    Personagem selecionado

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,9 +2,10 @@ let personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach( (personagem) => {
     personagem.addEventListener('mouseenter', () => {
-        personagem.classList.add('selecionado');
-
         let persSelecionado = document.querySelector('.selecionado');
         persSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado');
+
     })
 })
