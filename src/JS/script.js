31998d2c6a09502eea98e9fc378c1369c838a091
Junior7Marques <juniorjr07@live.com>
2023-07-09 16:43:05 -0300

    Final

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,18 +2,46 @@ let personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach( (personagem) => {
     personagem.addEventListener('mouseenter', () => {
-        let persSelecionado = document.querySelector('.selecionado');
-        persSelecionado.classList.remove('selecionado')
+
+        if (window.innerWidth < 450) {
+            window.scroll({top: 0, behavior: "smooth"});
+        }
+
+        removerSelecaoPersonagem();
 
         personagem.classList.add('selecionado');
 
-        let idPersonagem = personagem.attributes.id.value; 
+        alterarImagemSelecionada(personagem);
+        
+        alterarNomePersonagem(personagem);
 
-        let picGrande = document.querySelector('.personagem-grande');
-        
-        picGrande.src = `./src/imagens/card-${idPersonagem}.png`;
-        
+        alterarDescricaoPersonagem(personagem);
 
 
     })
 })
+
+function alterarDescricaoPersonagem(personagem) {
+    let descricao = document.getElementById("descricao-personagem");
+
+    descricao.innerText = personagem.getAttribute("data-description");
+}
+
+function alterarNomePersonagem(personagem) {
+    let nomegrande = document.getElementById("nome-personagem");
+
+    nomegrande.innerText = personagem.getAttribute("data-name");
+}
+
+function alterarImagemSelecionada(personagem) {
+    let idPersonagem = personagem.attributes.id.value;
+
+    let picGrande = document.querySelector('.personagem-grande');
+
+    picGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoPersonagem() {
+    let persSelecionado = document.querySelector('.selecionado');
+    persSelecionado.classList.remove('selecionado');
+}
